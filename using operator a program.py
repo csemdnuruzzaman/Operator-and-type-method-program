b256f7fc9300e834c25b31d9a432c24edd3e94f9
csemdnuruzzaman <nuruzzamanpolytechnic@gmail.com>
2023-03-06 16:47:20 +0600

    first commit

---
+++ using operator a program.py
@@ -0,0 +1,41 @@
+#Arithmetic Operator
+a = 10
+b = 21
+c = b+a
+print("The sum is",c)
+c = b-a
+print("The sub is",c)
+c = a*b
+print("The multi is ",c)
+c = b/a
+print("The div is",c)
+c = a%b
+print("The modulus is",c)
+c = a**b
+print("The power calcuation is",c)
+c = b//a
+print("The floor divi is",c)
+
+#comparison operator
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#Logical Operator
+print(a and b) # and means multiplication
+print(a or b) # or means summation
+print(not a) # true to false || false to true
+
+#Assignment Operator
+a+=5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+
---
+++ using type method program.py
@@ -0,0 +1,8 @@
+text ="Nuruzzaman"
+roll= 435296
+cgpa= 3.52
+dicision = True
+print(type(text))
+print(type(roll))
+print(type(cgpa))
+print(type(dicision))
